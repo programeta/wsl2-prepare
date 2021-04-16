baaca8b6cfa38b581ace9ec75f35af65a9f6ccca
Programeta <programeta@gmail.com>
2021-04-16 11:22:19 +0200

    Remove git autocompletition

--- prepare.sh
+++ prepare.sh
@@ -18,14 +18,6 @@ cat << EOF >> /home/dev/.bashrc
 
 cd /docker_projects
 
-parse_git_branch() {
-         git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
- }
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
-if [ -f ~/.git-completion.bash ]; then
-          . ~/.git-completion.bash
-fi
-
 sudo service docker start
 EOF
 
